061cdba798fb4600fb9775b6ea0307b77429063e
Pavlović Dž Filip <pavlovic.dz.filip@outlook.com>
2021-02-04 15:45:34 +0100

    message

--- TestApp/AuthenticationTest.cs
+++ TestApp/AuthenticationTest.cs
@@ -19,32 +19,69 @@ namespace TestApp
         {
             try
             {
-                await CreateUsernamePasswordAccount();
-                await Logout();
+                //for (int i=1;i<10;i++)
+                //{
+               // await CreateUsernamePasswordAccount();
+                 //await CreateEmailPasswordAccount(i);
+                 await CreateEmailPasswordAccount();
+                 await Logout();
+                 // await CreateUsernamePasswordAccount(); izmeniti da bude za token
+                 //await CreateUsernamePasswordAccount(); izmeniti da bude za async
+                //await Logout();
+               // }
+                
+                
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Error: {e.Message}");
+                Console.WriteLine($"Error: {e.Message}"); //probati sa Stacktrace
             }
         }
 
-        private async Task CreateUsernamePasswordAccount()
+        private async Task CreateUsernamePasswordAccount() 
         {
+            //var username = "J-123";
             var username = $"User-{Guid.NewGuid().ToString().Substring(0, 8)}";
             var result = await _service.AuthenticateWithUsernamePasswordAsync(
                 new AuthenticateWithUsernamePasswordLoginRequest
                 {
                     AppVersion = "AppVersion",
+                    //DisplayName = "User-123",
                     DisplayName = username,
                     SyncDisplayName = true,
                     Operation = AuthenticateOperation.LoginOrRegister,
                     Password = "pass",
+                    //Username = "Test4321"
                     Username = username
                 });
 
             Debug.Assert(result.IsSuccessful);
             Debug.Assert(result.Message == null);
             Console.WriteLine($"UserId: {result.Data.User.Id}");
+            Console.WriteLine("username: " + username);
+
+        }
+
+        private async Task CreateEmailPasswordAccount()
+        {
+            //var email = "test" + i + "@gmail.com";
+            var email = "testjgmail.com";
+            var result = await _service.AuthenticateWithEmailPasswordAsync(
+                new AuthenticateWithEmailPasswordLoginRequest
+                {
+                    AppVersion = "AppVersion",
+                    DisplayName = email,
+                    SyncDisplayName = true,
+                    Operation = AuthenticateOperation.LoginOrRegister,
+                    Password = "pass",
+                    Email = email
+                });
+
+            //Debug.Assert(result.IsSuccessful);  
+            Console.WriteLine(result.Message);  
+            Debug.Assert(result.Message == null);
+            Console.WriteLine($"UserId: {result.Data.User.Id}");
+            Console.WriteLine("Email: " + email);
         }
 
         private async Task Logout()
